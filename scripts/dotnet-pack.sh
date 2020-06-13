252d8e7f0aee7b11ed43abcf64705d779badc35e
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-13 22:33:09 +0430

    .

--- scripts/dotnet-pack.sh
+++ scripts/dotnet-pack.sh
@@ -2,16 +2,16 @@
 echo Executing after success scripts on branch $TRAVIS_BRANCH
 echo Triggering MyGet package build
 
-cd src/DShop.Common
-dotnet pack /p:PackageVersion=2.0.$TRAVIS_BUILD_NUMBER --no-restore -o .
+cd src/MicroBootstrap
+dotnet pack /p:PackageVersion=1.0.$TRAVIS_BUILD_NUMBER --no-restore -o .
 
-echo Uploading DShop.Common package to MyGet using branch $TRAVIS_BRANCH
+echo Uploading MicroBootstrap package to MyGet using branch $TRAVIS_BRANCH
 
 case "$TRAVIS_BRANCH" in
   "master")
-    dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/dnc-dshop/api/v2/package
+    dotnet nuget push *.nupkg -k $MYGET_API_KEY -s https://www.myget.org/F/micro-bootstrap/api/v3/index.json
     ;;
   "develop")
-    dotnet nuget push *.nupkg -k $MYGET_DEV_API_KEY -s https://www.myget.org/F/dnc-dshop-dev/api/v2/package
+    dotnet nuget push *.nupkg -k $MYGET_DEV_API_KEY -s https://www.myget.org/F/micro-bootstrap-dev/api/v3/index.json
     ;;    
-esac
+esacs
