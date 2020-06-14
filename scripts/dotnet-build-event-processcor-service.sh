38508430205088bb12b0f9f984c9a640541a9772
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-15 00:19:55 +0430

    .

--- scripts/dotnet-build-event-processcor-service.sh
+++ scripts/dotnet-build-event-processcor-service.sh
@@ -6,4 +6,4 @@
      ;;
  esac
 cd ../samples/Game-Microservices-Sample/Game.Services.EventProcessor/src/Game.Services.EventProcessor.API
-dotnet build -c Release --no-cache --source https://www.myget.org/F/micro-bootstrap/api/v3/index.json
+dotnet build -c Release --no-cache --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
--- scripts/dotnet-build-messaging-service.sh
+++ scripts/dotnet-build-messaging-service.sh
@@ -6,4 +6,4 @@
      ;;
  esac
 cd ../samples/Game-Microservices-Sample/Game.Services.Messaging/src/Game.Services.Messaging.API
-dotnet build -c Release --no-cache --source https://www.myget.org/F/micro-bootstrap/api/v3/index.json
+dotnet build -c Release --no-cache --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
