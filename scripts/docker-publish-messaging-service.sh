4e6d78bfaddfc405dca09ddc886528aac6a30937
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-16 00:31:47 +0430

    .

--- scripts/docker-publish-messaging-service.sh
+++ scripts/docker-publish-messaging-service.sh
@@ -9,7 +9,7 @@ case "$TRAVIS_BRANCH" in
     DOCKER_TAG=dev
     ;;    
 esac
-
+cd ../samples/Game-Microservices-Sample/Game.Services.Messaging
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 docker build -t game.services.messaging:$DOCKER_TAG .
 docker tag game.services.messaging:$DOCKER_TAG $DOCKER_USERNAME/game.services.messaging:$DOCKER_TAG
