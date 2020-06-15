0caeb315b113a612a9949a1a7e9df4042c7f47f8
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-16 00:26:16 +0430

    .

---
+++ scripts/docker-publish-event-processor-service.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+DOCKER_TAG=''
+
+case "$TRAVIS_BRANCH" in
+  "master")
+    DOCKER_TAG=latest
+    ;;
+  "develop")
+    DOCKER_TAG=dev
+    ;;    
+esac
+
+docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
+docker build -t game.services.event-processor:$DOCKER_TAG .
+docker tag game.services.event-processor:$DOCKER_TAG $DOCKER_USERNAME/game.services.event-processor:$DOCKER_TAG
+docker push $DOCKER_USERNAME/game.services.event-processor:$DOCKER_TAG
