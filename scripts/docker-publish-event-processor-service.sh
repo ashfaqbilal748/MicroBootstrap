6601370f1c15ffec0192bcbcf61721f114ff9f25
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-16 10:03:09 +0430

    .

--- scripts/docker-publish-event-processor-service.sh
+++ scripts/docker-publish-event-processor-service.sh
@@ -9,8 +9,7 @@ case "$TRAVIS_BRANCH" in
     DOCKER_TAG=dev
     ;;    
 esac
-cd ./samples/Game-Microservices-Sample/Game.Services.EventProcessor
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
-docker build -t game.services.event-processor:$DOCKER_TAG .
+docker build -f ./samples/Game-Microservices-Sample/Game.Services.EventProcessor/Dockerfile -t game.services.event-processor:$DOCKER_TAG .
 docker tag game.services.event-processor:$DOCKER_TAG $DOCKER_USERNAME/game.services.event-processor:$DOCKER_TAG
 docker push $DOCKER_USERNAME/game.services.event-processor:$DOCKER_TAG
--- scripts/dotnet-publish-event-processcor-service.sh
+++ scripts/dotnet-publish-event-processcor-service.sh
@@ -1,3 +1,2 @@
 #!/bin/bash
-cd ../samples/Game-Microservices-Sample/Game.Services.EventProcessor/src/Game.Services.EventProcessor.API
-dotnet publish  -c Release -o ./bin/Docker --source https://www.myget.org/F/micro-bootstrap/api/v3/index.json
+dotnet publish ./samples/Game-Microservices-Sample/Game.Services.EventProcessor/src/Game.Services.EventProcessor.API -c Release -o ./bin/Docker --source https://www.myget.org/F/micro-bootstrap/api/v3/index.json
