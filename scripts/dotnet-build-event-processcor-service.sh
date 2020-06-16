087e27d033e3b2c5d9b670bdd57fc03c3b0e9f9b
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-16 23:20:27 +0430

    .

--- scripts/dotnet-build-event-processcor-service.sh
+++ scripts/dotnet-build-event-processcor-service.sh
@@ -6,4 +6,4 @@
      ;;
  esac
 cd ./samples/Game-Microservices-Sample/Game.Services.EventProcessor/src/Game.Services.EventProcessor.API
-dotnet build -c Release --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v2/package 
+dotnet build -c Release --source https://api.nuget.org/v3/index.json --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
