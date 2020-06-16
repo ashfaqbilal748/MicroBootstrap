b56a0637f1fcd094d67cda8ab9f5727dfaed5515
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-16 22:27:18 +0430

    .

--- scripts/dotnet-publish-event-processcor-service.sh
+++ scripts/dotnet-publish-event-processcor-service.sh
@@ -7,5 +7,5 @@
      MYGET_ENV="-dev"
      ;;
  esac
-
-dotnet publish ./samples/Game-Microservices-Sample/Game.Services.EventProcessor/src/Game.Services.EventProcessor.API -c Release -o ./bin/Docker --no-cache --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
+cd samples/Game-Microservices-Sample/Game.Services.EventProcessor/src/Game.Services.EventProcessor.API
+dotnet publish  -c Release -o ./bin/Docker --no-cache --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
--- scripts/dotnet-publish-messaging-service.sh
+++ scripts/dotnet-publish-messaging-service.sh
@@ -6,4 +6,4 @@
      ;;
  esac
 
-dotnet publish ./samples/Game-Microservices-Sample/Game.Services.Messaging/src/Game.Services.Messaging.API -c Release -o ./bin/docker  --no-cache --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
+dotnet publish samples/Game-Microservices-Sample/Game.Services.Messaging/src/Game.Services.Messaging.API -c Release -o ./bin/docker  --no-cache --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
