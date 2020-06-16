ed08014d47fd2aefd8ac0038f05b2fac0652e371
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-16 21:49:12 +0430

    .

--- scripts/dotnet-publish-event-processcor-service.sh
+++ scripts/dotnet-publish-event-processcor-service.sh
@@ -1,2 +1,11 @@
 #!/bin/bash
-dotnet publish ./samples/Game-Microservices-Sample/Game.Services.EventProcessor/src/Game.Services.EventProcessor.API -c Release -o ./bin/Docker
+
+#!/bin/bash
+ MYGET_ENV=""
+ case "$TRAVIS_BRANCH" in
+   "develop")
+     MYGET_ENV="-dev"
+     ;;
+ esac
+cd ../samples/Game-Microservices-Sample/Game.Services.EventProcessor/src/Game.Services.EventProcessor.API
+dotnet publish -c Release -o ./bin/Docker --no-cache --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
--- scripts/dotnet-publish-messaging-service.sh
+++ scripts/dotnet-publish-messaging-service.sh
@@ -1,3 +1,9 @@
 #!/bin/bash
+ MYGET_ENV=""
+ case "$TRAVIS_BRANCH" in
+   "develop")
+     MYGET_ENV="-dev"
+     ;;
+ esac
 cd ../samples/Game-Microservices-Sample/Game.Services.Messaging/src/Game.Services.Messaging.API
-dotnet publish  -c Release -o ./bin/docker 
+dotnet publish  -c Release -o ./bin/docker  --no-cache --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
