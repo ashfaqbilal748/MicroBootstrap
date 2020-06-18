a8aeca20bc1c33791976dec9dc5db0bf114564ba
mehdihadeli <mehdi.hadeli@outlook.com>
2020-06-18 15:33:29 +0430

    .

--- scripts/dotnet-publish-messaging-service.sh
+++ scripts/dotnet-publish-messaging-service.sh
@@ -1,4 +1,4 @@
-
+#!/bin/bash
  MYGET_ENV=""
  case "$TRAVIS_BRANCH" in
    "develop")
@@ -6,4 +6,4 @@
      ;;
  esac
 
-dotnet publish ./samples/Game-Microservices-Sample/Game.Services.Messaging/src/Game.Services.Messaging.API -c Release -o ./bin/Docker  --source https://api.nuget.org/v3/index.json --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
+dotnet publish ./samples/Game-Microservices-Sample/Game.Services.Messaging/src/Game.Services.Messaging.API -c Release -o ./samples/Game-Microservices-Sample/Game.Services.Messaging/src/Game.Services.Messaging.API/bin/Docker  --source https://api.nuget.org/v3/index.json --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
