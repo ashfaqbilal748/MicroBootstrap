214ee0af10e56e13990c3550cad0796f40d9099b
mehdi hadeli <mehdi.hadeli@outlook.com>
2020-10-24 12:54:38 +0330

    .

--- scripts/dotnet-build-event-processcor-service.sh
+++ scripts/dotnet-build-event-processcor-service.sh
@@ -2,8 +2,8 @@
  MYGET_ENV=""
  case "$TRAVIS_BRANCH" in
    "develop")
-     MYGET_ENV="-dev"
+     MYGET_ENV="dev"
      ;;
  esac
 cd ./samples/Game-Microservices-Sample/Game.Services.EventProcessor/src/Game.Services.EventProcessor.API
-dotnet build -c Release --source https://api.nuget.org/v3/index.json --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
+dotnet build -p:VersionPrefix=1.0.0 -p:VersionSuffix=MYGET_ENV  -p:SourceRevisionId=$TRAVIS_JOB_ID -p:RepositoryType=git -p:RepositoryBranch=$TRAVIS_BRANCH -c Release --source https://api.nuget.org/v3/index.json --source https://www.myget.org/F/micro-bootstrap$MYGET_ENV/api/v3/index.json
